efff7b2f7bb80101fb5780fa91e7915801ef001c
marzo20 <myjune91@gmail.com>
2023-09-22 21:13:25 -0700

    working on firebase

--- App.js
+++ App.js
@@ -2,10 +2,18 @@ import React from "react";
 import { NavigationContainer } from '@react-navigation/native'
 import AppStackScreens from './src/stacks/AppStackScreens'
 
+import {UserProvider} from "./src/context/UserContext"
+import { FirebaseProvider } from "./src/context/FirebaseContext"
+
 export default App = () => {
   return(
-    <NavigationContainer>
-      <AppStackScreens /> 
-    </NavigationContainer>
+    <FirebaseProvider>
+      <UserProvider>
+        <NavigationContainer>
+          <AppStackScreens /> 
+        </NavigationContainer>
+      </UserProvider>
+
+    </FirebaseProvider>
   )
 }
---
+++ src/config.js/firebase.js
@@ -0,0 +1,8 @@
+export default{
+    apiKey: "AIzaSyBpsKx81n6ehVF3Q11kusaNjLtlDNtvKKQ",
+    authDomain: "pairplay-efa41.firebaseapp.com",
+    projectId: "pairplay-efa41",
+    storageBucket: "pairplay-efa41.appspot.com",
+    messagingSenderId: "708753247019",
+    appId: "1:708753247019:web:ee2b87c3dacb0be73c536e"
+}
